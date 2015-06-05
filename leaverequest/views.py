bf911690230d1836c2ef9738f63864ece409a8b0
Marek Suliga <marek.suliga@transparency.ge>
2015-06-05 13:59:52 +0400

    New leave request submission code

--- leaverequest/views.py
+++ leaverequest/views.py
@@ -1,65 +1,96 @@
 from django.shortcuts import render
 from employee.views import checkuserloggedin
-from timesheet import tshelpers
-from leaverequest import models
-import datetime
+from leaverequest.models import LeaveForm, Leave
+from django.utils.datetime_safe import datetime
+from publicholidays.models import PublicHoliday
+from datetime import timedelta
+from timesheet.tshelpers import send_notification
+
 
 @checkuserloggedin
 def request_leave( request, employee ):
 
-    time_sheet_data = tshelpers.generate_timesheet_data( employee )
+    message = ''
 
-    if request.method != "POST":
 
-        leave_form = models.LeaveForm()
+    if request.method == "POST":
+        leave_form = LeaveForm( request.POST )
+
+        if leave_form.is_valid():
+            
+            leave_request = Leave(
+                                  start_date = datetime.strptime( request.POST["start_date"], '%Y-%m-%d' ),
+                                  end_date = datetime.strptime( request.POST["end_date"], '%Y-%m-%d' ),
+                                  employee = employee,
+                                  type = request.POST["type"],
+                                  comment = request.POST["comment"],
+                                  submit_date = datetime.today(),
+                                  approved_by = employee.supervisor.first_name + ' ' + employee.supervisor.last_name
+                                  )
+
+            working_days = count_working_days( leave_request )
+
+            message = "%s Leave request for %d working days has been submitted for approval." % (
+                                                                                               dict( leave_request.TYPES)[ leave_request.type],
+                                                                                               working_days,
+                                                                                               )
+            
+            
+            hols_result = employee.leave_balance_HOLS
+            if leave_request.type == 'HOLS':
+                hols_result -= working_days
+                
+            sick_result = employee.leave_balance_SICK
+            if leave_request.type == 'SICK':
+                sick_result -= working_days
+            
+            
+            
+            message += '\nYour resulting balance will be Vacation: %d, Sick %.2f' % (hols_result, sick_result)
+            
+            leave_request.save()
+            
+            send_notification( request, "SUBMITTED", leave_request )
 
     else:
-
-        leave_form = models.LeaveForm( request.POST )
-        if  leave_form.is_valid():
-
-            leave_type = request.POST['type']
-            start_date = request.POST['start_date']
-            end_date = request.POST['end_date']
+        leave_form = LeaveForm()
 
 
-            date_start = [int( x ) for x in start_date.split( '-' )]
-            date_end = [int( x ) for x in end_date.split( '-' )]
-            date_start = datetime.date( date_start[0], date_start[1], date_start[2] )
-            date_end = datetime.date( date_end[0], date_end[1], date_end[2] )
-
-            if date_start > date_end:
-                return render( request, "request_leave.html", {"employee": employee,
-                                                               "form": leave_form,
-                                                               "timesheet": time_sheet_data,
-                                                               'message': "End date before Start date"} )
-# # commented out for the sake of demonstration
-#             if leave_type == 'HOLS':
-#                 if date_start < datetime.date.today() or date_end < datetime.date.today():
-#                     return render( request, "request_leave.html", {"employee": employee,
-#                                                                    "form": leave_form,
-#                                                                    "timesheet": time_sheet_data,
-#                                                                    "message": "Vacation can not begin or end in the past"} )
+    return render( request, "request_leave.html", {
+                                                   "employee": employee,
+                                                   "form": leave_form,
+                                                   "message": message,
+                                                   } )
 
 
-            comment = request.POST['comment']
-
-            leave_request = models.Leave( 
-                                         type = leave_type,
-                                         start_date = start_date,
-                                         end_date = end_date,
-                                         comment = comment,
-                                         submit_date = datetime.date.today(),
-                                         employee = employee )
-            leave_request.save()
-
-            tshelpers.send_notification( request, "SUBMITTED", leave_request )
-
-            return render( request, "time_sheet_front.html", {'employee': employee,
-                                                              'message' : 'Your leave request has been submitted for approval',
-                                                              "timesheet": time_sheet_data} )
+def count_working_days( leave_request ):
+    start_date = leave_request.start_date
+    end_date = leave_request.end_date
 
 
-    return render( request, "request_leave.html", {"employee": employee, "form": leave_form, "timesheet": time_sheet_data} )
+    holidays = PublicHoliday.objects.filter( date__gte = start_date
+                                             ).filter( date__lte = end_date)
 
-
+    holiday_count = 0
+    
+    for holiday in holidays:
+        if holiday.date.weekday() < 5 and holiday.type == "Public holiday":
+            holiday_count += 1
+        
+    current_date = start_date
+    
+    workday_count = 0
+    while True:
+        if current_date.weekday() < 5:
+            workday_count += 1
+            
+        if current_date == end_date:
+            break
+        
+        current_date += timedelta( days = 1) 
+        
+    
+    workday_count -= holiday_count
+    
+    return workday_count
+    
