3b3d3f0900e3f7592bee03d111ce3da75ece538e
Marek Suliga <marek.suliga@transparency.ge>
2015-06-11 16:32:07 +0400

    Front page redirection and small changes in presenting operations

--- OfficeManager/urls.py
+++ OfficeManager/urls.py
@@ -9,16 +9,21 @@ from leaverequest.views import request_leave
 from documentworkflow.views import list_requests_to_approve, approved_documents
 from employee.views import manage_profile
 from OfficeManager import settings
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = patterns( '',
     url( r'^admin/', include( admin.site.urls ) ),
-    url( r'^$', frontpage),
+#     url( r'^$', frontpage),
+#     url( r'^$', front_page, name = "front-page" ),
+    url( r'^$', RedirectView.as_view( url = reverse_lazy( 'time-sheet-front' ) ), name = 'front-page' ),
+
     url( r'^logout/$', sitelogout, name = "system-logout" ),
-    #url( r'^timesheet/', include( 'TimeSheetManager.urls') ),
-    url( r'^budgetshare/$', import_salary_assignments, name = "import-salary-assignments"),
-    url( r'^publicholidays/$', import_public_holidays, name = "import-public-holidays"),
-    
-    
+    # url( r'^timesheet/', include( 'TimeSheetManager.urls') ),
+    url( r'^budgetshare/$', import_salary_assignments, name = "import-salary-assignments" ),
+    url( r'^publicholidays/$', import_public_holidays, name = "import-public-holidays" ),
+
+
     url( r'^operations$', front_page, name = "time-sheet-front" ),
     url( r'^submittimesheet$', submit_time_sheet, name = "submit-time-sheet" ),
     url( r'^requestleave$', request_leave, name = "request-leave" ),
@@ -27,11 +32,11 @@ urlpatterns = patterns( '',
     #     url( r'^assignsalary$', assign_salary_sources, name = "assign-salary" ),
     #     url( r'^manageaccounts$', manage_users, name = "manage-accounts" ),
     url( r'^approveddocuments$', approved_documents, name = "approved-documents" ),
-    
-    url( r'^employeeprofile$', manage_profile, name = "employee-profile"),
-    
+
+    url( r'^employeeprofile$', manage_profile, name = "employee-profile" ),
+
     # this is necessary for Django server to serve mediafiles,notsure if this does the trick when deployed to Apache
-    url( r'^officemanager/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url( r'^officemanager/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT} ),
 
 
  )
